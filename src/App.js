f56d0c9270da164f303c8d604dbfb82cee56e7f8
afifi brahim <afifi71brahim@gmail.com>
2023-02-11 01:18:13 +0100

    fix the date

--- src/App.js
+++ src/App.js
@@ -10,12 +10,12 @@ const [timerSeconds,setTimerSecondes]= useState()
 
 let interval;
 const StartTimer =()=>{
-  const countDownDate = new Date('february 7,2023').getTime()
+  const countDownDate = new Date('july 30,2023').getTime()
   interval = setInterval(()=>{
     const now = new Date().getTime()
     const distance = countDownDate - now;
     const days = Math.floor(distance/(24*60*60*1000))
-    const hours = Math.floor(distance%(24*60*60*1000)/(1000*60*60))
+    const hours = Math.floor(distance % (24*60*60*1000)/(1000*60*60))
     const minutes = Math.floor(distance %(60*60*1000)/(1000*60))
     const secondes = Math.floor(distance % (60*1000)/(1000))
     if(distance<0){
@@ -31,7 +31,7 @@ const StartTimer =()=>{
 }
 useEffect(()=>{
   StartTimer()
-})
+},[])
   return (
     <div className="App">
     <Counter timerDays={timerDays} timerHours={timerHours} timerMinutes={timerMinutes} timerSeconds={timerSeconds}/>
